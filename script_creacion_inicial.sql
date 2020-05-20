e7942ccec278af49a9d87a4a5ac220db5220ab55
Franco Cavallini <43447255+francocava@users.noreply.github.com>
2020-05-20 12:58:45 -0300

    migro hoteles y pasajes

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -112,6 +112,30 @@ IF OBJECT_ID('EQUIPO_RAYO.Vuelos') IS NULL
 		vuelo_llegada DATETIME2(3)
 	)
 
+IF OBJECT_ID('EQUIPO_RAYO.Pasajes') IS NULL
+	CREATE TABLE EQUIPO_RAYO.Pasajes
+	(
+		pasaje_id INT IDENTITY PRIMARY KEY,
+		vuelo_id INT FOREIGN KEY (vuelo_id) REFERENCES EQUIPO_RAYO.Vuelos(vuelo_id) NOT NULL,
+		compra_id INT FOREIGN KEY (compra_id) REFERENCES EQUIPO_RAYO.Compras(compra_id) NOT NULL,
+		butaca_id INT FOREIGN KEY (butaca_id) REFERENCES EQUIPO_RAYO.Butacas(butaca_id) NOT NULL,
+		pasaje_codigo DECIMAL(18,0),
+		pasaje_costo DECIMAL(18,2),
+		pasaje_precio DECIMAL(18,2)
+	)
+
+
+IF OBJECT_ID('EQUIPO_RAYO.Hoteles') IS NULL
+	CREATE TABLE EQUIPO_RAYO.Hoteles
+	(
+		hotel_id INT IDENTITY PRIMARY KEY,
+		empresa_id INT FOREIGN KEY (empresa_id) REFERENCES EQUIPO_RAYO.Empresas(empresa_id) NOT NULL,
+		hotel_calle NVARCHAR(50),
+		hotel_nro_calle DECIMAL(18,0),
+		hotel_estrellas DECIMAL(18,0)
+	)
+
+
 --=========================Migracion==============================================================================================================================
 
 
@@ -179,6 +203,22 @@ SELECT R.ruta_id,A.avion_id, V.VUELO_CODIGO,V.VUELO_FECHA_SALUDA,V.VUELO_FECHA_L
 	INNER JOIN EQUIPO_RAYO.Aviones A ON A.avion_identificador = V.AVION_IDENTIFICADOR WHERE V.AVION_IDENTIFICADOR IS NOT NULL AND V.RUTA_AEREA_CODIGO IS NOT NULL
 GROUP BY R.ruta_id,A.avion_id, V.VUELO_CODIGO,V.VUELO_FECHA_SALUDA,V.VUELO_FECHA_LLEGADA
 
+--Pasajes --4 min en correr esto. Pasa que una compra puede ser uno o varios pasajes entonces genera muchas filas 
+INSERT INTO EQUIPO_RAYO.Pasajes(vuelo_id,compra_id,butaca_id,pasaje_codigo,pasaje_costo,pasaje_precio)
+SELECT V.vuelo_id,C.compra_id,B.butaca_id,P.PASAJE_CODIGO,P.PASAJE_COSTO,P.PASAJE_PRECIO FROM gd_esquema.Maestra P
+	INNER JOIN EQUIPO_RAYO.Vuelos V ON V.vuelo_codigo = P.VUELO_CODIGO
+	INNER JOIN EQUIPO_RAYO.Compras C ON C.compra_numero = P.COMPRA_NUMERO
+	INNER JOIN EQUIPO_RAYO.Butacas B ON B.butaca_numero = P.BUTACA_NUMERO 
+		WHERE P.VUELO_CODIGO IS NOT NULL AND P.COMPRA_NUMERO IS NOT NULL AND P.BUTACA_NUMERO IS NOT NULL 
+GROUP BY V.vuelo_id,C.compra_id,B.butaca_id,P.PASAJE_CODIGO,P.PASAJE_COSTO,P.PASAJE_PRECIO
+
+
+
+--Hoteles
+INSERT INTO EQUIPO_RAYO.Hoteles(empresa_id,hotel_calle,hotel_nro_calle,hotel_estrellas)
+SELECT E.empresa_id,H.HOTEL_CALLE,H.HOTEL_NRO_CALLE,H.HOTEL_CANTIDAD_ESTRELLAS FROM gd_esquema.Maestra H
+	INNER JOIN EQUIPO_RAYO.Empresas E ON E.empresa_razon_social = H.EMPRESA_RAZON_SOCIAL WHERE H.EMPRESA_RAZON_SOCIAL IS NOT NULL AND H.HOTEL_CALLE IS NOT NULL
+GROUP BY E.empresa_id,H.HOTEL_CALLE,H.HOTEL_NRO_CALLE,H.HOTEL_CANTIDAD_ESTRELLAS
 
 
 
@@ -192,7 +232,7 @@ DROP TABLE EQUIPO_RAYO.Compras
 DROP TABLE EQUIPO_RAYO.Sucursales
 DROP TABLE EQUIPO_RAYO.Tipos
 DROP TABLE EQUIPO_RAYO.Vuelos
-
+DROP TABLE EQUIPO_RAYO.Hoteles
 
 
 --Para probar si migro bien 
@@ -214,6 +254,9 @@ SELECT * FROM EQUIPO_RAYO.Sucursales
 
 SELECT * FROM EQUIPO_RAYO.Vuelos --todo ok
 SELECT DISTINCT vuelo_codigo FROM EQUIPO_RAYO.Vuelos
+
+SELECT * FROM EQUIPO_RAYO.Pasajes --2 min en devolverlo
+SELECT * FROM EQUIPO_RAYO.Hoteles
 
 
 SELECT DISTINCT SUCURSAL_MAIL FROM gd_esquema.Maestra 
